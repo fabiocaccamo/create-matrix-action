dd1b682d2e65dae6b429bb428852f83dd605820a
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-03-06 15:33:08 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from itertools import product
+
+import fsutil
+import os
+import re
+
+
+def main():
+    matrix_str = os.environ['INPUT_MATRIX_INPUT_STRING']
+    matrix_lines = list(filter(bool, [line.strip() for line in matrix_str.splitlines()]))
+    matrix_items = []
+    for line in matrix_lines:
+        matches = re.findall(r"(([\w\-]+){1}(?:\s+)?\{(.+?(?=\})){1}\})+", line)
+        assert len(matches)
+        groups = {}
+        for match in matches:
+            assert len(match) == 3
+            values = re.findall(r"([\w\-]+(?:\.[\d]+){0,2})+", match[2])
+            assert len(values)
+            groups[match[1]] = [{match[1]: value} for value in values]
+        for value in product(*groups.values()):
+            matrix_item = {}
+            for item in value:
+                matrix_item.update(item)
+            matrix_items.append(matrix_item)
+    fsutil.write_file_json(os.environ['INPUT_MATRIX_OUTPUT_JSON'], matrix_items)
+
+
+if __name__ == "__main__":
+    main()
