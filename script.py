39ebe29169f84dc1cca22e94181e87b7c8b32f95
Fabio Caccamo <fabio.caccamo@gmail.com>
2025-01-18 14:58:22 +0100

    Update script.

--- script.py
+++ script.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from itertools import product
 
 import json
@@ -7,11 +5,12 @@ import os
 import re
 
 
-def main():
-    matrix_str = os.environ["INPUT_MATRIX"]
-    matrix_lines = list(
-        filter(bool, [line.strip() for line in matrix_str.splitlines()])
-    )
+def parse_list(items):
+    return list(filter(bool, [item.strip() for item in items]))
+
+
+def parse_matrix(matrix_str):
+    matrix_lines = parse_list(matrix_str.splitlines())
     matrix_items = []
     for line in matrix_lines:
         matches = re.findall(r"(([\w\-]+){1}(?:\s+)?\{(.+?(?=\})){1}\})+", line)
@@ -19,14 +18,21 @@ def main():
         groups = {}
         for match in matches:
             assert len(match) == 3
-            values = re.findall(r"([\w\s\-]+(?:\.[\d]+){0,2})+", match[2])
+            values = parse_list(match[2].split(","))
             assert len(values)
-            groups[match[1]] = [{match[1]: value.strip()} for value in values]
+            group_name = match[1].strip()
+            groups[group_name] = [{group_name: value.strip()} for value in values]
         for value in product(*groups.values()):
             matrix_item = {}
             for item in value:
                 matrix_item.update(item)
             matrix_items.append(matrix_item)
+    return matrix_items
+
+
+def main():
+    matrix_str = os.environ["INPUT_MATRIX"]
+    matrix_items = parse_matrix(matrix_str)
     matrix_json = json.dumps(matrix_items)
     matrix_data = f"\nmatrix={matrix_json}\n".encode("utf-8")
     with open(os.environ["GITHUB_OUTPUT"], "ab") as file:
